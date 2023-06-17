446ca586fe2b5a5ade0994da0a6070e2917f0ab0
Daniel Sharifi <daniel.sharifi@dfinity.org>
2023-06-17 03:07:33 +0200

    Added -y flag to apt-get

--- src/fish/install.sh
+++ src/fish/install.sh
@@ -2,18 +2,18 @@
 set -e
 
 echo "Installing software-properties-common"
-apt-get install software-properties-common
+apt-get -y install software-properties-common
 
 echo "Adding fish-shell PPA"
 add-apt-repository ppa:fish-shell/release-3
 
 echo "Updating package list"
-apt-get update
+apt-get -y update
 
 echo "Updgrading packages"
-apt-get upgrade
+apt-get -y upgrade
 
 echo "Installing fish-shell"
-apt-get install fish
+apt-get -y install fish
 
 echo "Finished setting up Fish shell"
