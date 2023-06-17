2097b6287021b925e144b0bcf5a4fb0a9b7be778
Daniel Sharifi <daniel.sharifi@dfinity.org>
2023-06-17 02:46:06 +0200

    Changed id to simply 'fish'

--- src/fish/install.sh
+++ src/fish/install.sh
@@ -2,18 +2,18 @@
 set -e
 
 echo "Installing software-properties-common"
-sudo apt-get install software-properties-common
+apt-get install software-properties-common
 
 echo "Adding fish-shell PPA"
-sudo add-apt-repository ppa:fish-shell/release-3
+add-apt-repository ppa:fish-shell/release-3
 
 echo "Updating package list"
-sudo apt-get update
+apt-get update
 
 echo "Updgrading packages"
-sudo apt-get upgrade
+apt-get upgrade
 
 echo "Installing fish-shell"
-sudo apt-get install fish
+apt-get install fish
 
 echo "Finished setting up Fish shell"
